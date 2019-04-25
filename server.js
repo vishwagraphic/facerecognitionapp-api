56de6194a3c7e568b9885b47d51362b75f5347dc
Viswanathan Devarajan <viswanathanlakshmi@ViswanathansMBP.home>
2019-04-25 12:18:56 -0400

    Changes for server.js

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-const cool = require('cool-ascii-faces')
 const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
@@ -9,11 +8,6 @@ const register = require('./controller/Register');
 const signin = require('./controller/Signin');
 const profile = require('./controller/Profile');
 const image = require('./controller/Image');
-
-const path = require('path')
-const PORT = process.env.PORT || 5001
-
-
 
 const db = knex({
     client : 'pg',
@@ -29,12 +23,6 @@ const db = knex({
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
-app.use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .get('/cool', (req, res) => res.send(cool()))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 app.get('/', (req, res) => {res.send(database.users)});
 app.post('/signin', (req, res) => {signin.handleSignin(req, res, db, bcrypt)});
@@ -43,6 +31,7 @@ app.get('/profile/:id', (req, res) => {profile.handleProfile(req, res, db)});
 app.put('/image', (req, res) => {image.handleImage(req, res, db)})
 app.post('/imageUrl', (req, res) => {image.handleImageAPI(req, res)})
 
-/* app.listen(PORT, () => {
+const PORT = process.env.PORT;
+app.listen(PORT || 5000, () => {
     console.log(`App is running on ${PORT}`)
-}) */
+})
